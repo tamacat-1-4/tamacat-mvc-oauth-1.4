292fd17c870cb1cea2d5e2d6d3f34e9116704ef2
tamacat <tamacat@f3cb667f-c9e3-46e3-bc2c-302c0b7402fb>
2018-01-10 16:15:59 +0000

    moved
    git-svn-id: https://svn.code.sf.net/p/tamacat/code/branches/tamacat-mvc-oauth-1.4@1172 f3cb667f-c9e3-46e3-bc2c-302c0b7402fb

--- src/main/java/org/tamacat/mvc/servlet/RestfulApiServlet.java
+++ src/main/java/org/tamacat/mvc/servlet/RestfulApiServlet.java
@@ -4,7 +4,7 @@
  * Licensed under the Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0
  */
-package org.tamacat.mvc;
+package org.tamacat.mvc.servlet;
 
 import java.io.IOException;
 
